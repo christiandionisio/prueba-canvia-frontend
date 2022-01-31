3917986dbb6ea4afeb37413a703bfe1852bc32b1
Christian Dionisio <christiandionisio9432@gmail.com>
2022-01-30 23:37:19 -0500

    Mostrar facturas en tabla

--- src/app/pages/factura/factura.component.ts
+++ src/app/pages/factura/factura.component.ts
@@ -1,6 +1,9 @@
+import { SelectionModel } from '@angular/cdk/collections';
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { MatTableDataSource } from '@angular/material/table';
 import { FacturaDialigComponent } from 'src/app/components/factura-dialig/factura-dialig.component';
+import { Factura } from 'src/app/interfaces/factura.interface';
 import { FacturasService } from 'src/app/services/facturas.service';
 
 @Component({
@@ -10,10 +13,24 @@ import { FacturasService } from 'src/app/services/facturas.service';
 })
 export class FacturaComponent implements OnInit {
 
+  ELEMENT_DATA: Factura[] = [];
+
+  displayedColumns: string[] = [ 'idFactura', 'fechaEmision', 'estado', 'operaciones'];
+  dataSource = new MatTableDataSource<Factura>(this.ELEMENT_DATA);
+  selection = new SelectionModel<Factura>(true, []);
+
   constructor(private facturasService: FacturasService,
     public dialog: MatDialog) { }
 
   ngOnInit(): void {
+    this.mostrarFacturas();
+  }
+
+  mostrarFacturas(): void {
+    this.facturasService.getItems().subscribe((response: any) => {
+      this.ELEMENT_DATA = response;
+      this.dataSource = new MatTableDataSource<Factura>(this.ELEMENT_DATA);
+    });
   }
 
   openDialog(): void {
@@ -23,7 +40,18 @@ export class FacturaComponent implements OnInit {
     });
 
     dialogRef.afterClosed().subscribe(result => {
-      // this.mostrarItems();
+      this.mostrarFacturas();
+    });
+  }
+
+  verFactura(element: Factura) {
+    const dialogRef = this.dialog.open(FacturaDialigComponent, {
+      width: '700px',
+      data: { operacion: 'Ver', facturaConsolidado: element},
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      this.mostrarFacturas();
     });
   }
 
